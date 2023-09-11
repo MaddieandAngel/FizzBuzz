4ccc95f01a9568c6f6398306087b6d14b753496a
MaddieandAngel <madeleine.morais@mail.utoronto.ca>
2023-09-11 10:10:07 -0400

    Completed Task 4

--- src/Multiples.java
+++ src/Multiples.java
@@ -4,7 +4,7 @@ public class Multiples {
         for (int i=1; i <= 1000; i++) {
 
             if ((i % 3 == 0) || (i % 5 == 0)) {
-                System.out.print("\n" + i);
+                System.out.println(i);
             }
         }
     }
